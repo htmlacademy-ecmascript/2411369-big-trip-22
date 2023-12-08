bd208d0d1b08bf9078b5664f8e653d21642acdf0
Артур <artur.ayup@gmail.com>
2023-12-08 19:59:55 +0400

    Создаёт точку входа

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import FilterView from './view/filter-view.js';
+import InfoTripView from './view/info-trip-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
+import { render } from './render';
+
+const headerElement = document.querySelector('.page-header');
+const filterElement = headerElement.querySelector('.trip-controls__filters');
+const infoTripElement = headerElement.querySelector('.trip-main');
+const mainElement = document.querySelector('.page-main');
+const eventsTripElement = mainElement.querySelector('.trip-events');
+
+const boardPresenter = new BoardPresenter({boardPresenter: eventsTripElement});
+
+render(new InfoTripView(), infoTripElement, 'afterbegin');
+render(new FilterView(), filterElement);
+
+boardPresenter.init();
