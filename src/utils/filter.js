afd74efaf9d771b40a512c36f40b24aa596e1e6e
Артур <artur.ayup@gmail.com>
2024-01-21 15:35:39 +0400

    Добавляет презентер для фильтра и логику фильтрации

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,11 +1,11 @@
-import { FILTER_TYPE } from '../const';
+import { FilterType } from '../const.js';
 import { isPastEvent, isPresentEvent, isFutureEvent } from './point.js';
 
 const filter = {
-  [FILTER_TYPE.EVERYTHING]: (points) => points,
-  [FILTER_TYPE.FUTURE]: (points) => points.filter(isFutureEvent),
-  [FILTER_TYPE.PRESENT]: (points) => points.filter(isPresentEvent),
-  [FILTER_TYPE.PAST]: (points) => points.filter(isPastEvent),
+  [FilterType.EVERYTHING]: (points) => points,
+  [FilterType.FUTURE]: (points) => points.filter(isFutureEvent),
+  [FilterType.PRESENT]: (points) => points.filter(isPresentEvent),
+  [FilterType.PAST]: (points) => points.filter(isPastEvent)
 };
 
 export { filter };
