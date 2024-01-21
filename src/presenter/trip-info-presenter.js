1d6244ace45143ab50f814c75ef3f4fb2115ee55
Артур <artur.ayup@gmail.com>
2024-01-21 15:44:18 +0400

    Обновляет логику отрисовки верхнего блока информации

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -1,67 +1,44 @@
-import TripInfoView from '../view/trip-info-view';
-import TripInfoMainView from '../view/trip-info-main-view.js';
-import TripDatesView from '../view/trip-dates-view';
-import TripRouteView from '../view/trip-route-view';
-import TripTotalPriceView from '../view/trip-total-price-view';
-import { render } from '../framework/render.js';
-import { destinations } from '../mock/point';
+import TripInfoView from '../view/trip-info-view.js';
+import { remove, render, replace } from '../framework/render.js';
+import { sortByDay } from '../utils/point.js';
 
 export default class TripInfoPresenter {
   #tripInfoContainer = null;
   #pointsModel = null;
+  #offersByTypeModel = null;
+  #destinationsModel = null;
 
-  #tripInfoComponent = new TripInfoView();
-  #tripInfoMainComponent = new TripInfoMainView();
+  #tripInfoComponent = null;
 
-  #listPoints = [];
-
-  constructor({ tripInfoContainer, pointsModel }) {
+  constructor({ tripInfoContainer, pointsModel, offersByTypeModel, destinationsModel }) {
     this.#tripInfoContainer = tripInfoContainer;
     this.#pointsModel = pointsModel;
+    this.#offersByTypeModel = offersByTypeModel;
+    this.#destinationsModel = destinationsModel;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
   }
 
   init() {
-    this.#listPoints = [...this.#pointsModel.points];
+    const points = this.#pointsModel.points.sort(sortByDay);
+    const offersByType = this.#offersByTypeModel.offersByType;
+    const destinations = this.#destinationsModel.destinations;
 
-    render(this.#tripInfoComponent, this.#tripInfoContainer, 'afterbegin');
-    render(this.#tripInfoMainComponent, this.#tripInfoComponent.element);
-    this.#renderTotalPrice(this.#listPoints);
-    this.#renderTripRoute(this.#listPoints);
-    this.#renderTripDates(this.#listPoints);
+    const prevInfoComponent = this.#tripInfoComponent;
+    this.#tripInfoComponent = new TripInfoView(points, offersByType, destinations);
+
+    if (prevInfoComponent === null) {
+      render(this.#tripInfoComponent, this.#tripInfoContainer, 'afterbegin');
+      return;
+    }
+
+    replace(this.#tripInfoComponent, prevInfoComponent);
+    remove(prevInfoComponent);
   }
 
-  #renderTripRoute(points) {
-    const routeCities = [];
-
-    points.forEach((point) => {
-      const pointDestination = destinations.find((appointment) => point.destination === appointment.id);
-      routeCities.push(pointDestination.name);
-    });
-
-    const waypoints = Array.from(new Set(routeCities));
-    render(new TripRouteView(waypoints), this.#tripInfoMainComponent.element);
-  }
-
-  #renderTripDates(points) {
-    const datesFrom = [];
-    points.forEach((point) => {
-      datesFrom.push(point.dateFrom);
-    });
-
-    datesFrom.sort();
-
-    render(new TripDatesView(datesFrom), this.#tripInfoMainComponent.element);
-  }
-
-  #renderTotalPrice(points) {
-    let totalPrice = 0;
-
-    points.forEach((point) => {
-      totalPrice += point.basePrice;
-    });
-
-    render(new TripTotalPriceView(totalPrice), this.#tripInfoComponent.element);
-  }
+  #handleModelEvent = () => {
+    this.init();
+  };
 }
 
 
